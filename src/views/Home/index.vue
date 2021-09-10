0e02c2485d465f44a95eca9a01ad9067bd0e9e57
moneyinto <moneyinto@moneyintodeMacBook-Pro.local>
2021-09-10 15:45:43 +0800

    示例

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,16 +1,81 @@
 <template>
-    <div class="container">xx</div>
+    <div class="container">
+        <div class="header"></div>
+        <div class="content">
+            <div class="h-line"></div>
+            <Flex :flex="2" column>
+                <Flex :flex="2">
+                    1
+                </Flex>
+                <div class="v-line"></div>
+                <Flex :flex="6">
+                    2
+                </Flex>
+                <div class="v-line"></div>
+                <Flex :flex="4">
+                    3
+                </Flex>
+                <div class="v-line"></div>
+            </Flex>
+            <div class="h-line"></div>
+            <Flex :flex="3" column>
+                <Flex :flex="4">
+                    4
+                </Flex>
+                <div class="v-line"></div>
+                <Flex :flex="5">
+                    5
+                </Flex>
+                <div class="v-line"></div>
+                <Flex :flex="4">
+                    6
+                </Flex>
+                <div class="v-line"></div>
+            </Flex>
+            <div class="h-line"></div>
+            <Flex :flex="2" column>
+                <Flex :flex="4">
+                    7
+                </Flex>
+                <div class="v-line"></div>
+                <Flex :flex="5">
+                    8
+                </Flex>
+                <div class="v-line"></div>
+            </Flex>
+            <div class="h-line"></div>
+        </div>
+    </div>
 </template>
 
 <script>
+import Flex from "@/components/flex";
+
 export default {
-    name: "home"
+    name: "home",
+    components: {
+        Flex
+    }
 };
 </script>
 
 <style scoped>
-    .container {
-        overflow: hidden;
-        height: 100vh;
-    }
+.container {
+    overflow: hidden;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+.header {
+}
+.content {
+    flex: 1;
+    display: flex;
+}
+.v-line {
+    height: 0.2rem;
+}
+.h-line {
+    width: 0.2rem;
+}
 </style>
