de0dfe8e4d800e0561804baf301f62e636b99e30
wanghaiyang <1711387929@qq.com>
2021-09-13 14:50:28 +0800

    count

--- src/views/Left/components/countScroll.vue
+++ src/views/Left/components/countScroll.vue
@@ -1,80 +1,158 @@
 <template>
-    <div class="countToBox">
-        <div class="city_info">
-            <span class="city_name">苏州市</span>
-            <div class="schoolsum">
-                <CountTo class="school_count" :start-val="0" :end-val="1000" :duration="3000"></CountTo>
-                <span>学校总数</span>
-            </div>
-        </div>
-        <div class="personnel_info">
-            <div class="personnel">
-                <CountTo :start-val="0" :end-val="1000" :duration="3000"></CountTo>
-                <span>总学生人数</span>
-            </div>
-            <div  class="personnel">
-                <CountTo :start-val="0" :end-val="1000" :duration="3000"></CountTo>
-                <span>总教师人数</span>
-            </div>
-            <div  class="personnel">
-                <CountTo :start-val="0" :end-val="1000" :duration="3000"></CountTo>
-                <span>总职工人数</span>
-            </div>
-        </div>
+  <div class="countToBox">
+    <div class="city_info">
+      <span class="city_name" @click="onstart">苏州市</span>
+      <div class="schoolsum">
+        <CountTo
+          ref="example1"
+          class="school_count"
+          :start-val="CountOne.startval"
+          :end-val="998"
+          :duration="3000"
+        ></CountTo>
+        <span class="school_text">学校总数</span>
+      </div>
     </div>
+    <div class="personnel_info">
+      <div class="personnel">
+        <CountTo
+          ref="example2"
+          class="personnel_sum"
+          :start-val="0"
+          :end-val="1000"
+          :duration="3000"
+        ></CountTo>
+        <span class="personnel_text">总学生人数</span>
+      </div>
+      <div class="personnel">
+        <CountTo
+          ref="example3"
+          class="personnel_sum"
+          :start-val="0"
+          :end-val="1000"
+          :duration="3000"
+        ></CountTo>
+        <span class="personnel_text">总教师人数</span>
+      </div>
+      <div class="personnel">
+        <CountTo
+          ref="example4"
+          class="personnel_sum"
+          :start-val="0"
+          :end-val="1000000"
+          :duration="3000"
+        ></CountTo>
+        <span class="personnel_text">总职工人数</span>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import CountTo from 'vue-count-to'
+import CountTo from "vue-count-to";
 export default {
-    components:{
-        CountTo
+  components: {
+    CountTo,
+  },
+  data(){
+    return{
+        CountOne:{
+            startval:0,
+            endval:1000,
+            duration:3000
+        }
     }
-}
+  },
+  mounted() {
+    window.setInterval( ()=> {
+      setTimeout( ()=> {
+        this.$refs.example1.start();
+        this.$refs.example2.start();
+        this.$refs.example3.start();
+        this.$refs.example4.start();
+      }, 0);
+    }, 8000);
+  },
+  methods: {
+    onstart() {
+      this.$refs.example3.start();
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-span{
-    display: block;
+span {
+  display: block;
 }
-.countToBox{
-    width: 4.8rem;
-    height: 1.7rem;
-    margin: 0 auto;
-    background: #8cabd6;
-    .city_info{
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        .city_name{
-            font-size: 0.5rem;
-            font-family: PingFangSC-Semibold, PingFang SC;
-            font-weight: 600;
-            color: #ffffff;
-            line-height: 56px;
-            background: linear-gradient(180deg, #6AA8DE 0%, #DFF9FF 100%);
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent; 
-        }
-        // .schoolsum{
-        //     display: flex;
-        //     flex-direction: column;
-        // }
-        .school_count{
-            font-size: 0.3rem;
-            font-family: Bahnschrift_SemiBold;
-            color: #0DFFFF;
-            line-height: 29px;
-        }
+.countToBox {
+  width: 4.8rem;
+  height: 1.7rem;
+  margin: 0 auto;
+  background: #000916;
+  padding: 0.1rem;
+  //   box-sizing: border-box;
+  .city_info {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0.25rem;
+    .city_name {
+      flex: 2;
+      font-size: 0.5rem;
+      font-family: PingFangSC-Semibold, PingFang SC;
+      font-weight: 600;
+      color: #ffffff;
+      line-height: 0.7rem;
+      background: linear-gradient(180deg, #6aa8de 0%, #dff9ff 100%);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      margin-left: 0.3rem;
     }
-    .personnel_info{
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        .personnel{
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-        }
+    .schoolsum {
+      flex: 1;
+      margin-left: 0.5rem;
     }
+    .school_count {
+      font-size: 0.3rem;
+      font-family: Bahnschrift_SemiBold;
+      color: #0dffff;
+      margin-bottom: 0.05rem;
+      line-height: 0.3625rem;
+    }
+    .school_text {
+      font-size: 0.15rem;
+      font-family: PingFangSC-Regular, PingFang SC;
+      font-weight: 400;
+      color: #edf2f8;
+      line-height: 0.2125rem;
+    }
+  }
+  .personnel_info {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    .personnel:not(:last-child) {
+      border-right: 1px solid rgba(255, 255, 255, 0.1);
+    }
+    .personnel {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      margin-left: 0.3rem;
+      .personnel_sum {
+        font-size: 0.25rem;
+        font-family: Bahnschrift;
+        color: #70bfff;
+        line-height: 0.3rem;
+        margin-bottom: 0.1rem;
+      }
+      .personnel_text {
+        font-size: 0.125rem;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        color: #96acc3;
+        line-height: 0.175rem;
+      }
+    }
+  }
 }
-
 </style>
