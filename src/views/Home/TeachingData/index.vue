25728d3778ab56eba9341143f4df84975da473d3
moneyinto <moneyinto@moneyintodeMacBook-Pro.local>
2021-09-14 10:09:10 +0800

    学生数据和教研数据大屏结构

--- src/views/Home/TeachingData/index.vue
+++ src/views/Home/TeachingData/index.vue
@@ -2,24 +2,29 @@
     <div class="content">
         <div class="h-line"></div>
         <Flex :flex="1" column>
-            <Flex :flex="3"> </Flex>
+            <!-- 名师工作室 - 区级公开课 - - -  -->
             <div class="v-line"></div>
-            <Flex :flex="5"> </Flex>
-            <div class="v-line"></div>
-            <Flex :flex="5"> 3 </Flex>
-            <div class="v-line"></div>
-        </Flex>
-        <div class="h-line"></div>
-        <Flex :flex="2" column>
-            <Flex :flex="1"> 4 </Flex>
-        </Flex>
-        <div class="h-line"></div>
-        <Flex :flex="1" column>
-            <Flex :flex="3"> 7 </Flex>
-            <div class="v-line"></div>
-            <Flex :flex="5"> 8 </Flex>
-            <div class="v-line"></div>
-            <Flex :flex="5"> 8 </Flex>
+            <Flex :flex="1">
+                <Flex :flex="1" column>
+                    <!-- 教师教研参与情况 -->
+                    <div class="v-line"></div>
+                    <Flex :flex="1"> </Flex>
+                </Flex>
+                <div class="h-line"></div>
+                <Flex :flex="2" column>
+                    <Flex :flex="2"> 4 </Flex>
+                    <div class="v-line"></div>
+                    <Flex :flex="1"> 
+                        <!-- 活跃度 -->
+                    </Flex>
+                </Flex>
+                <div class="h-line"></div>
+                <Flex :flex="1" column>
+                    <!-- 文字泡 -->
+                    <div class="v-line"></div>
+                    <Flex :flex="1"> 8 </Flex>
+                </Flex>
+            </Flex>
             <div class="v-line"></div>
         </Flex>
         <div class="h-line"></div>
