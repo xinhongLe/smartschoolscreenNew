3882d0666224cfdfbfb554ed4b67b70b16dfb2f1
jijie <jijie18465183529@163.com>
2021-09-17 10:20:09 +0800

    表现数据

---
+++ src/views/studentData/behavior/index.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="img-content">
+        <div class="h-line"></div>
+        <Flex :flex="1" column>
+            <Flex :flex="1"> <ClassDistribution></ClassDistribution> </Flex>
+            <div class="v-line"></div>
+            <Flex :flex="1"> </Flex>
+            <div class="v-line"></div>
+        </Flex>
+        <div class="h-line"></div>
+        <Flex :flex="1" column>
+            <Flex :flex="1"> </Flex>
+            <div class="v-line"></div>
+            <Flex :flex="1"> </Flex>
+            <div class="v-line"></div>
+            <Flex :flex="1"> </Flex>
+            <div class="v-line"></div>
+        </Flex>
+        <div class="h-line"></div>
+        <Flex :flex="1" column>
+            <Flex :flex="5"> </Flex>
+            <div class="v-line"></div>
+            <Flex :flex="5"> </Flex>
+            <div class="v-line"></div>
+        </Flex>
+        <div class="h-line"></div>
+        <Flex :flex="1" column>
+            <Flex :flex="5"> </Flex>
+            <div class="v-line"></div>
+            <Flex :flex="5"> </Flex>
+            <div class="v-line"></div>
+        </Flex>
+        <div class="h-line"></div>
+    </div>
+</template>
+<script>
+import Flex from "@/components/flex";
+import ClassDistribution from "./components/ClassDistribution.vue";
+export default {
+    data() {
+        return {
+            style: {}
+        };
+    },
+    components: { Flex,ClassDistribution },
+    methods: {
+        imgLoad() {
+            const imgScale =
+                this.$refs.img.clientWidth / this.$refs.img.clientHeight;
+            const scale =
+                this.$refs.content.clientWidth /
+                this.$refs.content.clientHeight;
+            if (imgScale > scale) {
+                this.style = { width: "100%" };
+            } else {
+                this.style = { height: "100%" };
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+/* .img-content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.img-content img {
+    display: block;
+} */
+.img-content {
+    flex: 1;
+    display: flex;
+    min-height: 0;
+}
+.v-line {
+    height: 0.4rem;
+}
+.h-line {
+    width: 0.4rem;
+}
+</style>
